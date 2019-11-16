f8a1b8ae5c6f8b86b56919c77e27cbc7807b1a38
Kacie Cooper <kcrouch@pdx.edu>
2019-11-15 18:21:11 -0800

    adding mobile menu

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,38 @@ $(function() {
 // Your Code from here on down. Don't delete that line above!
   
 
+const nav = document.querySelector('#nav');
+const menu = document.querySelector('#menu');
+const menuToggle = document.querySelector('.nav__toggle');
+let isMenuOpen = false;
 
 
+// TOGGLE MENU ACTIVE STATE
+menuToggle.addEventListener('click', e => {
+  e.preventDefault();
+  isMenuOpen = !isMenuOpen;
+  
+  // toggle a11y attributes and active class
+  menuToggle.setAttribute('aria-expanded', String(isMenuOpen));
+  menu.hidden = !isMenuOpen;
+  nav.classList.toggle('nav--open');
+});
 
-
+  
+  // listen for tab press and move focus
+  // if we're on either end of the navigation
+  const menuLinks = menu.querySelectorAll('.nav__link');
+  if (e.keyCode === 9) {
+    if (e.shiftKey) {
+      if (document.activeElement === menuLinks[0]) {
+        menuToggle.focus();
+        e.preventDefault();
+      }
+    } else if (document.activeElement === menuToggle) {
+      menuLinks[0].focus();
+      e.preventDefault();
+    }
+  }
 
 // End of Your Code . Don't delete that line below!!
 });
